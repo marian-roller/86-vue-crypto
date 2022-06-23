c00340b0787cb1a9a58e4d983124808e78cd9794
marian-roller <webmaster@rollercoastery.pl>
2022-06-23 22:17:33 +0200

    finish errors handling in register form view

--- src/store/auth.js
+++ src/store/auth.js
@@ -25,9 +25,10 @@ export default {
     actions: {
         async register({dispatch}, credentials) {
             let response = await axios.post('auth/register', credentials);
-            console.log(response.data)
+            return response.data
+            //console.log(response.data)
             //@TODO: login user if succesfully registered
-            return true;
+            // return true;
         },
         async signIn({dispatch}, credentials) {
             let response = await axios.post('auth/signin', credentials);
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -9,18 +9,34 @@
                     </div>
 
                     <div class="card-body">
-                        <form @submit.prevent="submit">
+                        <form @submit.prevent="submit" class="needs-validation">
                             <div class="form-group row">
                                 <label class="col-md-2" for="name">Name</label>
                                 <input class="form-control col-md-10" type="text" id="name" name="name" v-model="form.name">
+                                <div v-if="this.form.error.name" class="col-md-10 offset-md-2">
+                                    <span class="is-valid text-danger">
+                                        <small>{{ this.form.error.name[0] }}</small>
+                                    </span>
+                                </div>
                             </div>
+
                             <div class="form-group row">
                                 <label class="col-md-2" for="email">Email</label>
                                 <input class="form-control col-md-10" type="email" id="email" name="email" v-model="form.email">
+                                <div v-if="this.form.error.email" class="col-md-10 offset-md-2">
+                                    <span class="is-valid text-danger">
+                                        <small>{{ this.form.error.email[0] }}</small>
+                                    </span>
+                                </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-md-2" for="password">Password</label>
                                 <input class="form-control col-md-10" type="password" id="password" name="password" v-model="form.password">
+                                <div v-if="this.form.error.password" class="col-md-10 offset-md-2">
+                                    <span class="is-valid text-danger">
+                                        <small>{{ this.form.error.password[0] }}</small>
+                                    </span>
+                                </div>
                             </div>
 
                             <div class="row">
@@ -28,7 +44,6 @@
                                     <button class="btn btn-primary form-control" type="submit">Register</button>
                                 </div>
                             </div>
-                            
                             
                         </form>
                     </div>
@@ -50,8 +65,9 @@ import { mapActions } from 'vuex'
             form: {
                 name: '',
                 email: '',
-                password: ''
-            }
+                password: '',
+                error: {}
+            },
         }
     },
     methods: {
@@ -64,21 +80,10 @@ import { mapActions } from 'vuex'
                 this.$router.replace({
                     name: 'dashboard'
                     })
-            }).catch(() => {
-                // handle behaviour, validation on register failed
-                console.log('register failed')
+            }).catch((e) => {
+                this.form.error = e.response.data.error
             })
-
-
-            // let response = await axios.post('auth/register', this.form)
-            // console.log(response.data);
         }
-
-
-        // async submit() {
-        //     let response = await axios.post('auth/register', this.form)
-        //     console.log(response.data);
-        // }
     }
 }
 </script>
