463500ea7b7430153b1e79e42ca5a823b7c401b9
marian-roller <webmaster@rollercoastery.pl>
2022-06-24 23:43:37 +0200

    adjustments of hash layout

--- src/components/Hash.vue
+++ src/components/Hash.vue
@@ -2,15 +2,45 @@
     <div>
         <section class="mt-4">
             <div class="card">
+
+
                 <div class="card-header text-center">
-                    <h4>Hash generator</h4>
+
+                    <div class="row">
+
+                        <div class="col-md-2 text-left">
+                            <small>Algorithm: </small>
+                        </div>
+
+                        <div class="col-md-2">
+                            <select id="algorithm" class="form-control form-control-sm" v-model="form.algorithm">
+                                <option :value="null" disabled selected>Choose...</option>
+                                <option v-for="item in this.algorithms" :value="item">{{item}}</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6">
+                            <h4>Hash generator</h4>
+                        </div>
+
+                        <div class="col-md-2">
+                            <div class="btn btn-secondary btn-sm d-block" @click="clearForm">Reset</div>
+                        </div>
+                    
+                    </div>
                 </div>
+
+
+
+
+
+
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-1">
-                            <p>data:</p>
+                        <div class="col-md-2">
+                            <div class="font-weight-bold">data:</div>
                         </div>
-                        <div class="col-md-11">
+                        <div class="col-md-10">
                             <textarea 
                             id="input"
                             class="form-control" 
@@ -21,10 +51,10 @@
                         </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-1">
-                            <p>hash:</p>
+                        <div class="col-md-2">
+                            <div class="font-weight-bold">hash:</div>
                         </div>
-                        <div class="col-md-11">
+                        <div class="col-md-10">
                             <input 
                             id="hash"
                             type="text"
@@ -36,7 +66,14 @@
                     </div>
                 </div>
                 <div class="card-footer text-muted text-right">
-                    <small>Some minor action</small>
+                    <div class="row">
+                        <div class="col-md-2 text-left">
+                            <small>Present algorithm:</small>
+                        </div>
+                        <div class="col-md-10 text-left">
+                            <small class="text-danger">{{ form.algorithm }}</small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -45,6 +82,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'hash',
     data() {
@@ -55,6 +93,10 @@ export default {
                 salt: null,
                 hashed: null,
                 hash: null
+            },
+            algorithms: {
+                md5: 'md5',
+                sha1: 'sha1',
             }
         }
     },
@@ -62,15 +104,18 @@ export default {
         sendToConvert() {
             axios.post('hash/convert', 
             {
-                algorithm: md5,
+                algorithm: 'md5',
                 input: this.form.input,
+                // salt: this.form.salt
             })
             .then((response) => {
                 this.form.hash = response.data.result
             })
         },
         clearForm(event) {
-            this.form.hashed = '';
+            this.form.algorithm = null;
+            this.form.input = null;
+            this.form.hash = null;
         }
     }
 }
