ed905fa32d8d197a52cd9cbcbd9e35ddc9c7108b
marian-roller <webmaster@rollercoastery.pl>
2022-07-02 09:52:59 +0200

    prepare bootstrap layout for crypt basic sections

--- src/components/crypt/CryptDemo.vue
+++ src/components/crypt/CryptDemo.vue
@@ -1,5 +1,60 @@
 <template>
     <div>
-        Crypt demo vue
+        <section class="mt-4">
+            <div class="card">
+
+                <div class="card-header text-center">
+
+                    <div class="row">
+                        <div class="col">
+                            <h4>AES Encryption / Decryption Demo</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body p-0">
+                    <div class="row">
+                    
+                        <div class="col-md-6 pr-0">
+                            <div class="card">
+                                <div class="card-header">
+                                    Encryption
+                                </div>
+                                <div class="card-body">
+                                   
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 pl-0">
+                            <div class="card">
+                                <div class="card-header">
+                                    Decryption
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>  
+                </div>
+                
+                <div class="card-footer text-muted text-right">
+                    <div class="row">
+                        <div class="col-md-2 text-left">
+                            <small>Crypting scenario:</small>
+                        </div>
+                        <div class="col-md-10 text-left">
+                            <small class="text-danger">text / file</small>
+                            <small class=""> mode</small>
+                            <small class=""> key size</small>
+                            <small class=""> key</small>
+                            <small class=""> output format</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
